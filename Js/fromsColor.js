f7696596ba9124cc7d3def2c40f555ec418e727b
Mehdi_ha01@itstep.edu.az <Mehdi_ha01@itstep.edu.az>
2023-10-19 19:57:53 +0400

    guncel color

---
+++ Js/fromsColor.js
@@ -0,0 +1,25 @@
+
+
+btnId.addEventListener('click',function () {
+    let rId=document.getElementById('rID').value
+    let gId=document.getElementById('gID').value
+    let bId=document.getElementById('bID').value
+    if(rId !== "" && gId !== "" && bId !== ""){
+        const divAdd=document.getElementById('divAdd');
+        const newColor=document.createElement('input');
+        newColor.type='color';
+        newColor.value=rgbToHex(rId,gId,bId)
+        divAdd.appendChild(newColor)
+        let text = document.createTextNode(` R: ${rId} G: ${gId} B: ${bId}  `)
+        divAdd.appendChild(text)
+    }
+
+})
+function rgbToHex(r, g, b) {
+    const toHex = (value) => {
+        const hex = parseInt(value).toString(16);
+        return hex.length === 1 ? "0" + hex : hex;
+    };
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+}
+
